8b184843ba9ab2d1a5a965e2880bf553abd932f8
choclitoxd <victorm.ramirezc@uqvirtual.edu.co>
2026-04-18 15:15:02 -0500

    Cargue plugin para que lea el .env

--- src/main/java/com/universidad/pisc/Application.java
+++ src/main/java/com/universidad/pisc/Application.java
@@ -1,5 +1,6 @@
 package com.universidad.pisc;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		// Cargar variables de entorno desde el archivo .env
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(Application.class, args);
 	}
 
